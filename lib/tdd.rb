49252904a2ea3f13b0b7ea96f9daf38c97cf18e3
Neel <nkachalia1@gmail.com>
2023-09-15 15:21:25 -0400

    finished two sum

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,5 +1,5 @@
-class Shoe 
-    attr_reader :arr
+class Shoe
+    attr_accessor :arr
     def initialize(arr)
         @arr = arr
     end
@@ -11,6 +11,19 @@ class Shoe
                 new_arr << ele
             end
         end
+        new_arr
+    end
+
+    def two_sum
+        new_arr = []
+        (0...arr.length-1).each do |i1|
+            (i1+1...arr.length).each do |i2|
+                if arr[i1] + arr[i2] == 0
+                    new_arr << [i1, i2]
+                end
+            end
+        end
+
         new_arr
     end
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -15,5 +15,13 @@ describe 'Shoe' do
             expect(shoe.my_uniq).to eq([1, 2, 3])
         end
     end
-end
 
+    describe '#two_sum' do
+        before(:each) do
+            shoe.arr = [-1, 0, 2, -2, 1]
+        end
+        it 'should return an array of the paired indices that sum to zero' do
+            expect(shoe.two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+end
